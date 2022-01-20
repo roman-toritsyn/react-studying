fb9a6b6af9e8921af0d147b9df1e6ce8f7c75e68
roto <roto@opinov8.com>
2022-01-20 12:20:07 +0200

    uncontrolled and controlled

--- src/components/UncontrolledInput/UncontrolledInput.jsx
+++ src/components/UncontrolledInput/UncontrolledInput.jsx
@@ -1,40 +1,93 @@
-import React, { useRef, useState } from "react"
-
-// file input can not be controlled !!!
+import React, { useRef, useState, useEffect } from "react"
 
 export const UncontrolledInput = () => {
-  const [input, setInput] = useState('');
-  const inputRef = useRef(null);
+  const [inputData, setInputData] = useState({
+    name: '',
+    lastName: '',
+  })
 
-  const changeHandler = (event) => {
-    setInput(event.target.value);
+  const uncontrolledRef = useRef([])
+
+  const addInput = el => {
+    if (el && !uncontrolledRef.current.includes(el)) {
+      uncontrolledRef.current.push(el)
+    }
   }
 
-  const handleSubmit = (event) => {
+  useEffect(() =>{
+    console.log(uncontrolledRef);
+  }, [])
+
+  const changeInputHandler = (event) => {
+    if(event.target.name === 'name') {
+      setInputData({
+        name: event.target.value,
+        lastName: inputData.lastName,
+      })
+    }
+
+    if(event.target.name === 'lastName') {
+      setInputData({
+        name: inputData.name,
+        lastName: event.target.value,
+      })
+    }
+  }
+
+  const submitHandler = (event) => {
     event.preventDefault()
-    alert(input)
+
+    console.log(`uncontrolledInputOne: ${uncontrolledRef.current[0].value} uncontrolledInputTwo: ${uncontrolledRef.current[1].value}`);
+  }
+
+  const handleClick = (event) => {
+    const { name, lastName } = inputData;
+    alert(`${name} ${lastName}`)
   }
 
   return (
-    <form action="">
-      <div> inputRefValue: {`${inputRef?.current?.value}`}</div>
-
+    <form action="" onSubmit={submitHandler}>
       <input
         type="text"
-        value={input}
-        name="UserName"
-        onChange={changeHandler}
-        ref={inputRef}
+        value={inputData.name}
+        name="name"
+        onChange={changeInputHandler}
         placeholder="write your name"
         style={{marginBottom: '10px'}}
       >
       </input>
 
-      <br />
-      <input type="file" style={{marginBottom: '10px'}} />
+      <input
+        type="text"
+        value={inputData.lastName}
+        name="lastName"
+        onChange={changeInputHandler}
+        placeholder="write your lastname"
+        style={{marginBottom: '10px'}}
+      >
+      </input>
+
+      <input
+        type="text"
+        name="uncontrolledOne"
+        placeholder="uncontrolled"
+        style={{marginBottom: '10px'}}
+        ref={addInput}
+      >
+      </input>
+
+      <input
+        type="text"
+        name="uncontrolledTwo"
+        placeholder="uncontrolled"
+        style={{marginBottom: '10px'}}
+        ref={addInput}
+      >
+      </input>
 
       <br/>
-      <button onClick={handleSubmit}> Submit</button>
+      <button onClick={handleClick}> Alert controled inputs </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
