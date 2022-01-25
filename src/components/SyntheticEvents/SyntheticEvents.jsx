5d259e5cfb21b54d6b2d75b4f6be12bb6f7c4ac5
roto <roto@opinov8.com>
2022-01-25 18:28:31 +0200

    someFix

--- src/components/SyntheticEvents/SyntheticEvents.jsx
+++ src/components/SyntheticEvents/SyntheticEvents.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export const SyntheticEvents = () => {
   const [input, setInput] = useState('')
+  const [hoveredCount, setHovereCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `${hoveredCount}`
+  })
 
   return (
     <>
       <div
         style={{margin: '25px'}}
         onCopy={(e) => console.log(`${e.target} copied || nativeEvent is ${e.nativeEvent}`)}
-        onMouseOver={(e) => console.log(`mouse is over the div || X = ${e.clientX} Y = ${e.clientY}`)}
+        onMouseOver={(e) => {
+          console.log(`mouse is over the div || X = ${e.clientX} Y = ${e.clientY}`)
+          setHovereCount((prev) => prev + 1)
+        }}
         onMouseOut={() => console.log('mouse left the div')}
         onDoubleClick={() => console.log('mouse doubleClicked the div')}
         onContextMenu={() => console.log('context menu')}
